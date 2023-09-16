38be9548e36a0e99c0849d52d59d079c72c9eeb9
leroysb <leroybuliro@gmail.com>
2023-09-16 14:12:43 +0300

    base vendors and users routes update

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import VendorsController from '../controllers/VendorsController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+// Vendors routes
+router.post('/vendors', VendorsController.postVendor);
+router.get('/vendors/me', VendorsController.getMe);
+// Users routes
+router.post('/users', UsersController.postUser);
+router.get('/users/me', UsersController.getMe);
 
 export default router;
